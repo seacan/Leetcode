29107e8a43585d4e8d03e68f6f67a6d30be62bd6
seacan <seacan@seacan.com>
2015-04-06 22:56:13 -0700

    Leetcode 198-199 code checkin

---
+++ 198. House Robber.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int rob(int[] num) {
+        if (num.length == 0) return 0;
+        if (num.length == 1) return num[0];
+        int[] cache = new int[num.length];
+        cache[0] = num[0];
+        cache[1] = num[0] > num[1] ? num[0] : num[1];
+        for (int i = 2; i < num.length; i++)
+            cache[i] = Math.max(cache[i - 2] + num[i], cache[i-1]);
+        return cache[num.length - 1];
+    }
+}
---
+++ 199. Binary Tree Right Side View.java
@@ -0,0 +1,34 @@
+// Solution 1: divide and conquer
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null) return new ArrayList<Integer>();
+        List<Integer> left = rightSideView(root.left);
+        List<Integer> right = rightSideView(root.right);
+        List<Integer> res = new ArrayList<Integer>();
+        res.add(root.val);
+        for(int i=0;i<Math.max(left.size(), right.size());i++){
+            if(i>=right.size())
+                res.add(left.get(i));
+            else
+                res.add(right.get(i));
+        }
+        return res;
+    }
+}
+
+//Solution 2: 
+public class Solution {
+    public void levelOrder(TreeNode root, int currentLevel, List<Integer> rideSideNodes) {
+		if (root == null) return;
+		if (currentLevel > rideSideNodes.size()) rideSideNodes.add(root.val);
+		levelOrder(root.right, currentLevel + 1, rideSideNodes);
+		levelOrder(root.left, currentLevel + 1, rideSideNodes);
+	}
+
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> rideSideNodes = new ArrayList<Integer>();
+		int currentLevel = 1;
+		levelOrder(root, currentLevel, rideSideNodes);
+		return rideSideNodes;
+	}
+}
