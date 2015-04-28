e0d761d56b318723ddab65c674a5e5c2a134fdcd
seacan <seacan@seacan.com>
2015-04-27 23:26:58 -0700

    CC150 Get Nth number code update

--- CC150. Get Nth number.java
+++ CC150. Get Nth number.java
@@ -1,18 +1,18 @@
 public int FindNthNumber(int[] num, int start, int end, int rank) {
     int pivot = num[end];
-    int left = 0, right = end - 1;
+    int right = end - 1;
     for (int i = start; i <= right; i++) {
-        if (num[i] <= pivot) left++;
-        else {
+        if (num[i] > pivot) {
             int temp = num[right];
             num[right--] = num[i];
             num[i--] = temp;
         }
     }
+    right++;
     int temp = num[end];
-    num[end] = num[left];
-    num[left] = temp;
-    if (left == rank - 1) return num[left];
-    else if (left > rank - 1) return FindNthNumber(num, start, left - 1, rank);
-    else return FindNthNumber(num, left + 1, end, rank - left - start - 1);
+    num[end] = num[right];
+    num[right] = temp;
+    if (right == start + rank - 1) return num[right];
+    else if (right > start + rank - 1) return FindNthNumber(num, start, right - 1, rank);
+    else return FindNthNumber(num, right + 1, end, rank - (right - start + 1));
 }
