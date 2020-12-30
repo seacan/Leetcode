4e2f9878a00b6963964f39c15c61f29fa796a79a
seacan <weiwu427@gmail.com>
2020-12-29 22:17:07 -0800

    924,1219 code update

--- 1219. Path with Maximum Gold.java
+++ 1219. Path with Maximum Gold.java
@@ -6,25 +6,26 @@
 //  [0,9,0]]
 // Path to get the maximum gold, 9 -> 8 -> 7.
 class Solution {
-    private static final int[] d = {0, 1, 0, -1, 0};
+    private final int[] d = {0, 1, 0, -1, 0};
 
     public int getMaximumGold(int[][] grid) {
         int ans = 0;
-        Set<Integer> visited = new HashSet<Integer>();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; ++i)
             for (int j = 0; j < grid[0].length; ++j)
                 ans = Math.max(ans, dfs(grid, i, j, visited));
         return ans;
     }
 
-    private int dfs(int[][] g, int i, int j, Set<Integer> seen) {
-        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] == 0 || !seen.add(i * g[0].length + j))
+    private int dfs(int[][] g, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] == 0 || visited[i][j])
             return 0;
+        visited[i][j] = true;
         int cur = g[i][j];
         int mx = 0;
         for (int k = 0; k < 4; ++k)
-            mx = Math.max(mx, dfs(g, i + d[k], j + d[k + 1], seen));
-        seen.remove(i * g[0].length + j);  // Do not forget to remove.
+            mx = Math.max(mx, dfs(g, i + d[k], j + d[k + 1], visited));
+        visited[i][j] = false;
         return cur + mx;
     }
 }
